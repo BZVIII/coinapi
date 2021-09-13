df4e20b524a862a65aae1040cc60e80e469b35ed
Ramón Maldonado Rodríguez <monterdi@gmail.com>
2021-09-14 00:37:29 +0200

    Fin MVC con tkinter

--- criptovalue/__init__.py
+++ criptovalue/__init__.py
@@ -1,2 +1,9 @@
 APIKEY = "23DBFF71-249C-46BB-BB56-9EBDFA5399F6"
 URL = "https://rest.coinapi.io/v1/exchangerate/{}/{}"
+
+MONEDAS = ["EUR - Euro", 
+            "USD - Dolar",
+            "BTC - Bitcoin",
+            "ETH - Ethereum",
+            "BNB - Binance coin",
+            "ADA - Cardano"]
